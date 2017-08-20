2bf6800f0f69b9f190a282ea82103fcf173dc741
Vadim Macagon <vadim.macagon@gmail.com>
2017-08-20 17:56:06 +0700

    Fix alignment of links in the PostNav component

--- themes/react/layout/src/components/post_nav.tsx
+++ themes/react/layout/src/components/post_nav.tsx
@@ -8,18 +8,18 @@ interface IPostNavProps {
 
 const PostNav: React.SFC<IPostNavProps> = (props) => {
   const { page, url_for, fa_icon } = props;
-  const prevPage = page.prev ?
-    <a href={url_for(page.prev.path)}
+  const prevPage = page.prev &&
+    <a href={url_for(page.prev.path)} className="pre"
       dangerouslySetInnerHTML={
         {__html: fa_icon('caret-left', { width: '16px', height: '16px', ariaHidden: true }) + page.prev.title}
       }
-    /> : null;
-  const nextPage = page.next ?
-    <a href={url_for(page.next.path)}
+    />;
+  const nextPage = page.next &&
+    <a href={url_for(page.next.path)} className="next"
       dangerouslySetInnerHTML={
-        {__html: fa_icon('caret-right', { width: '16px', height: '16px', ariaHidden: true }) + page.next.title}
+        {__html: page.next.title + fa_icon('caret-right', { width: '16px', height: '16px', ariaHidden: true })}
       }
-    /> : null;
+    />;
   return (
     <div className="post-nav">
       { prevPage }
